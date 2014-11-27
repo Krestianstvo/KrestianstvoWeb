5c5e39867f407bd2ef43a0e06230e7633c987029
Nikolay Suslov <nsuslovi@gmail.com>
2014-11-28 01:51:50 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    h5bp = require('h5bp');
+
+var app = express();
+app.use(h5bp({ root: __dirname + '/public' }));
+
+// in order to serve files, you should add the two following middlewares
+app.use(express.compress());
+app.use(express.static(__dirname + '/public'));
+app.listen(3000);
