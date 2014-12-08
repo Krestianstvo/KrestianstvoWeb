0393e844a6c004d67105c21c61c9c3a0278b95b8
Nikolay Suslov <nsuslovi@gmail.com>
2014-12-08 04:22:46 +0300

    express server

--- app-express.js
+++ app-express.js
@@ -2,9 +2,27 @@ var express = require('express'),
 h5bp = require('h5bp');
 
 var app = express();
+var port = 3001;
 app.use(h5bp({ root: __dirname + '/public' }));
 
 // in order to serve files, you should add the two following middlewares
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+
+app.get('/en', function(req, res) {
+  res.sendfile(__dirname + '/public/index-en.html');
+});
+
+/* app.get('/vwf-ometa', function(req, res) {
+  res.sendfile(__dirname + '/public/vwf-ometa.html');
+});
+*/
+app.get('/vwf-ometa', function(req, res) {
+  res.redirect('projects/en/vwf-ometa');
+});
+
+app.listen(port);
+console.log('Krestianstvo Web server is started on port: '+ port);
+
+
+///redirection from old site paths http://www.krestianstvo.org/web/
