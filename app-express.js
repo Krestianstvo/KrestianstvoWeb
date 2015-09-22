193ad8d8e2a3720e0772a913f6e75bc9f1932d5a
Nikolay Suslov <nsuslovi@gmail.com>
2015-09-23 00:02:04 +0300

    fix

--- app-express.js
+++ app-express.js
@@ -58,7 +58,7 @@ app.get('/%D0%B4%D0%B0%D0%BB%D0%B5%D0%B5/krestianstvo', function(req, res) {
 
 
 app.get('/web/nikolaysuslov', function(req, res) {
-  res.redirect('http://krestianstvo.org/books/krestianstvo/ru/authors/suslovnv/INDEX.html');
+  res.redirect('../books/krestianstvo/ru/authors/suslovnv/INDEX.html');
 });
 
 
